943e6524aa6a1fedb36c19fbaee813fe3724ba76
tomni <tom.nicholson1000@gmail.com>
2019-10-31 11:04:30 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,93 @@
+let width = 400
+let height = 0
+let streaming = false
+
+const video = document.getElementById('video')
+const canvas = document.getElementById('canvas')
+const photos = document.getElementById('photos')
+const photoButton = document.getElementById('photo-button')
+const clearButton = document.getElementById('clear-button')
+const sentence = document.getElementById('sentence-swap')
+const swap = document.getElementById('swap')
+
+navigator.mediaDevices.getUserMedia({video: true})
+    .then(stream => {
+        video.srcObject = stream
+        video.play()
+    })
+    .catch(err => { console.log(`Error: ${err}`)})
+
+function takePicture() {
+    context = canvas.getContext('2d')
+
+    if (height && width) {
+        canvas.width = width / 2
+        canvas.height = height / 2
+    }
+    context.drawImage(video, 0, 0, width / 2, height / 2)
+
+    imgUrl = canvas.toDataURL('image/png')
+
+    image = document.createElement('img')
+
+    image.setAttribute('src', imgUrl)
+
+    photos.appendChild(image)
+}
+let counter = 0
+
+function timer() {
+    counter = 0
+    clearCanvas()
+    let pictureReel = setInterval(() => {
+        takePicture();
+
+        counter++
+        if (counter >= 10) {
+            clearInterval(pictureReel)
+        }
+    }, 1000)
+}
+
+function clearCanvas() {
+    photos.innerHTML = '';
+    counter = 0
+}
+
+const tongueTwisters = [`"Peter Piper picked a peck of pickled peppers. 
+A peck of pickled peppers Peter Piper picked. If Peter Piper picked a peck of pickled peppers? 
+Where's the peck of pickled peppers Peter Piper picked?"`, `"How much wood would a woodchuck chuck 
+if a woodchuck could chuck wood? He would chuck, he would, as much as he could, and chuck as much wood,
+as a woodchuck would if a woodchuck could chuck wood."`, `"Betty bought a bit of butter.
+But the butter Betty bought was bitter.
+so Betty bought a better butter,
+and it was better than the butter Betty bought before."`, `"Silly Sally swiftly shooed seven silly sheep.
+The seven silly sheep Silly Sally shooed
+Shilly-shallied south."`]
+
+function swapSentence() {
+    let randInt = Math.floor(Math.random() * tongueTwisters.length)
+    console.log(randInt)
+    sentence.textContent = tongueTwisters[randInt]
+}
+
+video.addEventListener('canplay', function(e) {
+    if (!streaming) {
+        height = video.videoHeight / (video.videoWidth / width)
+
+        video.style.height = height + 'px'
+        video.style.width = width + 'px'
+
+        streaming = true
+    }
+}, false)
+
+photoButton.addEventListener('click', function(e) {
+    timer()
+    e.preventDefault()
+})
+
+clearButton.addEventListener('click', clearCanvas)
+
+swap.addEventListener('click', swapSentence)
+photoButton.addEventListener('click', swapSentence)
